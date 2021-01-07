7c430ce9b6eccf05a0addfe4a319f7601fc04871
Dylan Zenner <zennerdylan23@gmail.com>
2021-01-07 11:44:25 -0600

    Added export_to_s3.py

---
+++ src/export_to_s3.py
@@ -0,0 +1,28 @@
+import boto3
+
+# Make our DynamoDB connection
+dynamo = boto3.client("dynamodb", "us-east-1")
+
+
+# Get dynamodb table ARN from SecretsManager
+def get_table_arn():
+    """
+    Retrieves the DynamoDB table ARN from SecretsManager
+    :return: Secret Table ARN
+    """
+    client = boto3.client("secretsmanager", "us-east-1")
+    response = client.get_secret_value(SecretId="dynamodb_arn")
+    return response["SecretString"]
+
+
+# Export the data to S3
+def lambda_handler(event, context):
+    """
+    Exports the data from DynamoDB to S3
+    :param event:
+    :param context:
+    :return: None
+    """
+    dynamo.export_table_to_point_in_time(
+        TableArn=get_table_arn(), S3Bucket="greenhouse-gas-emissions"
+    )
